a3d6d75a0b7a28fb07810d15fcb02a6931844166
Syntax Ngo <hoangnguyen.dev2003@gmail.com>
2026-01-14 21:31:16 +0700

    feat: Complete UI for ChatWindowHeader, ChatWelcomeScreen

--- frontend/src/components/chat/ChatWelcomeScreen.tsx
+++ frontend/src/components/chat/ChatWelcomeScreen.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { SidebarInset } from "../ui/sidebar";
+import ChatWindowHeader from "./ChatWindowHeader";
 
 const ChatWelcomeScreen = () => {
   return (
-    <div>ChatWelcomeScreen</div>
-  )
-}
+    <SidebarInset className="flex w-full h-full bg-transparent">
+      <ChatWindowHeader />
+      <div className="flex bg-primary-foreground rounded-2xl flex-1 items-center justify-center">
+        <div className="text-center">
+          <div className="size-24 mx-auto mb-6 bg-primary rounded-full flex items-center justify-center shadow-glow pulse-ring">
+            <span className="text-3xl">💬</span>
+          </div>
+          <h2 className="text-2xl font-bold mb-2 bg-primary bg-clip-text text-transparent">
+            Chào mừng bạn đến với KageChat
+          </h2>
+          <p className="text-muted-foreground">
+            Chọn một cuộc hội thoại để bắt đầu chat!
+          </p>
+        </div>
+      </div>
+    </SidebarInset>
+  );
+};
 
-export default ChatWelcomeScreen
+export default ChatWelcomeScreen;
--- frontend/src/components/chat/ChatWindowHeader.tsx
+++ frontend/src/components/chat/ChatWindowHeader.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import type { Conversation } from "@/types/chat";
+import { useChatStore } from "@/stores/useChatStore";
+import { SidebarTrigger } from "../ui/sidebar";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { Separator } from "../ui/separator";
+import UserAvatar from "./UserAvatar";
+import StatusBadge from "./StatusBadge";
+import GroupChatAvatar from "./GroupChatAvatar";
 
-const ChatWindowHeader = () => {
+const ChatWindowHeader = ({ chat }: { chat?: Conversation }) => {
+  const { conversations, activeConversationId } = useChatStore();
+  const { user } = useAuthStore();
+  let otherUser;
+  chat =
+    chat ?? conversations.find((conv) => conv._id === activeConversationId);
+
+  if (!chat) {
+    return (
+      <header className="md:hidden sticky top-0 z-10 flex items-center gap-2 px-4 py-2 w-full">
+        <SidebarTrigger className="-ml-1 text-foreground" />
+      </header>
+    );
+  }
+
+  if (chat.type === "direct") {
+    const otherUsers = chat.participants.filter(
+      (participant) => participant._id !== user?._id
+    );
+    otherUser = otherUsers.length > 0 ? otherUsers[0] : null;
+    if (!user || !otherUser) return;
+  }
+
   return (
-    <div>ChatWindowHeader</div>
-  )
-}
+    <header className="sticky top-0 z-10 px-4 py-2 flex items-center bg-background">
+      <div className="flex items-center gap-2 w-full">
+        <SidebarTrigger className="-ml-1 text-foreground" />
+        <Separator
+          orientation="vertical"
+          className="mr-2 data-[orientation=vertical]:h-4"
+        />
 
-export default ChatWindowHeader
+        <div className="p-2 w-full flex items-center gap-3">
+          {/* avatar */}
+          <div className="relative">
+            {chat.type === "direct" ? (
+              <>
+                <UserAvatar
+                  type={"sidebar"}
+                  name={otherUser?.displayName || "Moji"}
+                  avatarUrl={otherUser?.avatarUrl || undefined}
+                />
+                {/* todo: socket io */}
+                <StatusBadge
+                  status="offline"
+                />
+              </>
+            ) : (
+              <GroupChatAvatar
+                participants={chat.participants}
+                type="sidebar"
+              />
+            )}
+          </div>
+
+          {/* name */}
+          <h2 className="font-semibold text-foreground">
+            {chat.type === "direct" ? otherUser?.displayName : chat.group?.name}
+          </h2>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default ChatWindowHeader;
